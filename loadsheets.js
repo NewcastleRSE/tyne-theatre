91478f2bfae25617d95de65919ed39e81acbf0b5
KateCourt <49239607+KateCourt@users.noreply.github.com>
2023-06-22 08:25:49 +0100

    display sheet in simple table

--- loadsheets.js
+++ loadsheets.js
@@ -12,12 +12,55 @@ $.getJSON(
 
   // parse data from Sheets API into JSON
   var parsedData = Papa.parse(Papa.unparse(data['values']), {header: true} ).data
+  console.log(parsedData)
+createTable(parsedData)
 
 
-  
-console.log(data)
+
 }
 
 )
 
 })
+
+
+function createTable(jsonData) {
+    // EXTRACT VALUE FOR HTML HEADER.
+    var col = [];
+    for (var i = 0; i < jsonData.length; i++) {
+        for (var key in jsonData[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+    var tr = table.insertRow(-1);                   // TABLE ROW.
+
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0; i < jsonData.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = jsonData[i][col[j]];
+        }
+    }
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("showData");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+}
